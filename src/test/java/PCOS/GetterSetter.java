f121b33fb6ca921399a8709f2c6f98d818039a3c
testninjaproject <testninjaproject>
2023-12-16 12:34:50 -0800

    frist commit

---
+++ src/test/java/PCOS/GetterSetter.java
@@ -0,0 +1,30 @@
+package PCOS;
+
+import org.openqa.selenium.WebElement;
+
+public class GetterSetter {
+	private String recipeName;
+	private String recipeID;
+	private WebElement gotoRecipeDetails;
+	public String getRecipeName() {
+		return recipeName;
+	}
+	public void setRecipeName(String recipeName) {
+		this.recipeName = recipeName;
+	}
+	public String getRecipeID() {
+		return recipeID;
+	}
+	public void setRecipeID(String recipeID) {
+		this.recipeID = recipeID;
+	}
+	public WebElement getGotoRecipeDetails() {
+		return gotoRecipeDetails;
+	}
+	public void setGotoRecipeDetails(WebElement gotoRecipeDetails) {
+		this.gotoRecipeDetails = gotoRecipeDetails;
+		
+	}
+	}
+
+	
